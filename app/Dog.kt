b889eaf673d134aee69a5ef506e4630e1f219bee
Rudy Picard <carpi.rudy.lr@gmail.com>
2021-06-18 11:52:30 +0200

    code cleanup

--- app/Dog.kt
+++
@@ -1,9 +0,0 @@
-/*
-*   Created by Rudy PICARD
-*   Kotlin  Dog shelter project
-*
-* */
-package app
-
-data class Dog(val name: String, val color: String, val breed : String, val alimentation: String) {
-}
